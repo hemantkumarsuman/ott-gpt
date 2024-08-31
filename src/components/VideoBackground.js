dfcc3f09625581cac643b38f2e68feb4cbc79288
hemantkumarsuman <hemantkumar1699@gmail.com>
2024-08-31 13:52:42 +0530

    MainConainer built

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -27,8 +27,8 @@ const VideoBackground = ({movieId}) => {
   },[]);
 
   return (
-    <div>
-      <iframe width="560" height="315" src={"https://www.youtube.com/embed/"+trailerId} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin"></iframe>
+    <div className="w-screen">
+      <iframe className="w-screen aspect-video" src={"https://www.youtube.com/embed/"+trailerId+'?autoplay=1&mute=1'} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin"></iframe>
     </div>
   )
 };
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,11 +2,11 @@
 
 const VideoTitle = ({title,overview}) => {
   return (
-    <div className="pt-40 px-16">
+    <div className="w-screen aspect-video pt-[20%] px-16 absolute text-white bg-gradient-to-r from-black">
       <h1 className="text-5xl font-bold">{title}</h1>
       <p className="py-6 text-lg w-1/4">{overview}</p>
       <div>
-        <button className="text-white bg-gray-600 py-2 px-4 rounded-lg">▶️Play</button>
+        <button className="text-black bg-white py-2 px-4 rounded-lg hover:bg-opacity-75">▶️Play</button>
         <button className="text-white bg-gray-600 py-2 px-4 rounded-lg mx-4">❕More Info</button>
       </div>
     </div>
